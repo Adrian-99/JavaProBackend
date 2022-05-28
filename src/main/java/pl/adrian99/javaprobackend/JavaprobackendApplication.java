d1c33fd6f45cfa428e4ba591ecb1a9194cbcdbc5
Adrian-99 <adrian.jurek.1109@gmail.com>
2022-05-28 13:31:01 +0200

    Initial commit

---
+++ src/main/java/pl/adrian99/javaprobackend/JavaprobackendApplication.java
@@ -0,0 +1,13 @@
+package pl.adrian99.javaprobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaprobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaprobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/adrian99/javaprobackend/JavaprobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adrian99.javaprobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
